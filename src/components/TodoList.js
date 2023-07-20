beb0dc22051be1977b3ee33b6065ca9029b30485
b3nj4hb <73666409+b3nj4hb@users.noreply.github.com>
2023-07-20 14:26:18 -0500

    Initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = ({ todos, todoDelete, todoToogleCompleted }) => {
+
+    return (
+        <div>
+            <h1 className='text-end'>Soy TodoList</h1>
+
+            {
+                todos.map(todo => (
+                    <Todo
+                        todo={todo}
+                        key={todo.id}
+                        todoDelete={todoDelete}
+                        todoToogleCompleted={todoToogleCompleted}
+                    />
+                ))
+            }
+
+        </div>
+    );
+}
+
+export default TodoList;
